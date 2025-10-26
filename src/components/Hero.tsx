6b717ca2fa292448340c7be94916f1dbf66b6312
Sharjeel <sharjeel.asif66@gmail.com>
2025-10-26 21:54:13 +0500

    update hero leading

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import "@/index.css";
 export default function Hero() {
   return (
     <div className="hero-bg min-h-[90vh] relative overflow-hidden flex flex-col items-center bg-transparent justify-center text-primary-foreground">
-      <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl md:max-w-2xl font-bold relative z-10 max-w-md text-center leading-12.5">
+      <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl md:max-w-2xl font-bold relative z-10 max-w-md text-center leading-extratight">
         Computer Vision Edge Detection Project
       </h1>
       <p className="absolute bottom-3 right-4 text-primary-foreground/70">
